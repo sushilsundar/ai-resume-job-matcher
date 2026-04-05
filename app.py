497dcca4cd99befe0bdbe79d5f66d910ca6b5f3e
sushilsundar <bensushil48@gmail.com>
2026-04-05 17:18:56 +1000

    change

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-print("Project started successfully 🚀")
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+with open("resume.txt", "r") as file:
+    resume_text = file.read()
+
+prompt = f"""
+You are a career coach.
+
+Analyze this resume for software/data roles.
+
+Give:
+1. Strengths
+2. Missing skills
+3. Specific improvements
+
+Resume:
+{resume_text}
+"""
+
+response = client.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=[
+        {"role": "user", "content": prompt}
+    ]
+)
+
+print(response.choices[0].message.content)
